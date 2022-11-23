1b15cdf5bb9d6d31129308925de9649e0aa46fe2
VishalChoudhary <vishalghn953@gmail.com>
2022-11-23 22:27:20 +0530

    DB url update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://vishal_chy01:B1Wc4sDOtfh1HsRe@cluster0.o8zayda.mongodb.net/?retryWrites=true&w=majority").then(()=> {
+mongoose.connect("mongodb+srv://vishal_chy01:pz5mwtg7NmHPY1Vu@cluster0.d8chuvr.mongodb.net/?retryWrites=true&w=majority").then(()=> {
     console.log("Database Successfully connected");
 }).catch((err)=>{
     console.log("Error connecting to Database");
