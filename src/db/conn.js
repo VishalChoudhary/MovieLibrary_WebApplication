5460e98cf641445dcff0c71fe12c18bec33c30a4
VishalChoudhary <vishalghn953@gmail.com>
2023-08-23 19:35:50 +0530

    mongo local server update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,16 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb+srv://vishal_chy01:pz5mwtg7NmHPY1Vu@cluster0.d8chuvr.mongodb.net/?retryWrites=true&w=majority").then(()=> {
+//     console.log("Database Successfully connected");
+// }).catch((err)=>{
+//     console.log("Error connecting to Database");
+//     console.log(err)
+// })
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://vishal_chy01:pz5mwtg7NmHPY1Vu@cluster0.d8chuvr.mongodb.net/?retryWrites=true&w=majority").then(()=> {
+mongoose.connect("mongodb://0.0.0.0:27017/pr5").then(() => {
     console.log("Database Successfully connected");
-}).catch((err)=>{
-    console.log("Error connecting to Database");
-    console.log(err)
+}).catch((err) => {
+    console.log(err);
 })
